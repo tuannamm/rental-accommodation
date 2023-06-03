c4a33e8453f38073013c55fa1a817053629d4af2
Tuan Nam <nttnnam@gmail.com>
2023-06-03 15:32:11 +0700

    insert data category for db

--- server/src/migrations/create-category.js
+++ server/src/migrations/create-category.js
@@ -15,7 +15,10 @@ module.exports = {
       value: {
         type: Sequelize.STRING,
       },
-      subtitle: {
+      header: {
+        type: Sequelize.STRING,
+      },
+      subheader: {
         type: Sequelize.STRING,
       },
       createdAt: {
